dd33d9057c47cee7c9e8500685a28f3b7b58bddd
jaeyo_on <jyo3on@gmail.com>
2018-02-09 15:53:46 +0900

    change token

--- build.sh
+++ build.sh
@@ -10,22 +10,22 @@ fi
 set -e
 
 # build site with jekyll, by default to `_site' folder
-jekyll build
+bundle exec jekyll build
 
 # cleanup
-rm -rf ../eshepelyuk.github.io.master
+rm -rf ../jyoonsong.github.io.master
 
 #clone `master' branch of the repository using encrypted GH_TOKEN for authentification
-git clone https://${GH_TOKEN}@github.com/eshepelyuk/eshepelyuk.github.io.git ../eshepelyuk.github.io.master
+git clone https://${GH_TOKEN}@github.com/jyoonsong/jyoonsong.github.io.git ../jyoonsong.github.io.master
 
 # copy generated HTML site to `master' branch
-cp -R _site/* ../eshepelyuk.github.io.master
+cp -R _site/* ../jyoonsong.github.io.master
 
 # commit and push generated content to `master' branch
 # since repository was cloned in write mode with token auth - we can push there
-cd ../eshepelyuk.github.io.master
-git config user.email "eshepelyuk@gmail.com"
-git config user.name "Evgeny Shepelyuk"
+cd ../jyoonsong.github.io.master
+git config user.email "jyo3on@gmail.com"
+git config user.name "jyoonsong"
 git add -A .
 git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
 git push --quiet origin master > /dev/null 2>&1

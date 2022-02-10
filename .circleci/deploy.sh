babca623ba1eaa971902e916185d3d1d610c12f3
jyoonsong <jaeyoons@mit.edu>
2022-02-10 04:57:43 -0500

    change branch name

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -9,7 +9,7 @@ mv _site/* .
 rm -R _site/
 
 git add -fA
-git commit --allow-empty -m "$(git log source -1 --pretty=%B)"
+git commit --allow-empty -m "$(git log jekyll -1 --pretty=%B)"
 git push -f origin master
 
 echo "deployed successfully"
